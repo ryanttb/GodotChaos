2688a53d0be0d45907109ce13da5dd41688bedc3
ryanttb <ryan@trippingthebits.com>
2025-02-25 16:24:57 -0500

    town attack wizard

--- townattack/princess.gd
+++ townattack/princess.gd
@@ -2,12 +2,23 @@ extends Area2D
 
 class_name PrincessNode
 
+const SPEED := 400.0
+
 const BOUNDS_MIN := Vector2(10, 10)
 const BOUNDS_MAX := Vector2(544 - 10, 960 - 10)
 
+const BOUNDS := Rect2(Vector2(10, 10), Vector2(544 - 20, 960 - 20))
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction != Vector2.ZERO:
+		position.x = clampf(position.x + direction.x * SPEED * delta, BOUNDS_MIN.x, BOUNDS_MAX.x)
+		position.y = clampf(position.y + direction.y * SPEED * delta, BOUNDS_MIN.y, BOUNDS_MAX.y)
+	
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("tap") and is_position_event(event):
+	if is_position_event(event):
 		position.x = clampf(event.position.x, BOUNDS_MIN.x, BOUNDS_MAX.x)
+		position.y = clampf(event.position.y, BOUNDS_MIN.y, BOUNDS_MAX.y)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Enemies"):
@@ -15,7 +26,7 @@ func _on_area_entered(area: Area2D) -> void:
 		main.game_over()
 
 func is_position_event(event: InputEvent) -> bool:
-	return event is InputEventMouseButton or event is InputEventScreenDrag or event is InputEventScreenTouch
+	return event is InputEventMouse or event is InputEventScreenDrag or event is InputEventScreenTouch
 
 func print_input_event(event: InputEvent) -> void:
 	print("Princess print_input_event position: ", event.position)
